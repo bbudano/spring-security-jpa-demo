4e8f8a232f487f48c3c2e3441e4dbef2cacb623e
Bernard Budano <bbudano@gmail.com>
2022-12-26 15:25:28 +0100

    Removed unnecessary dto

--- src/main/java/com/example/springsecurityjpademo/mapper/RoleMapper.java
+++ src/main/java/com/example/springsecurityjpademo/mapper/RoleMapper.java
@@ -1,13 +1,9 @@
 package com.example.springsecurityjpademo.mapper;
 
-import com.example.springsecurityjpademo.dto.CreateRoleRequest;
 import com.example.springsecurityjpademo.dto.RoleDto;
-import com.example.springsecurityjpademo.dto.UserProfileDto;
 import com.example.springsecurityjpademo.model.Role;
-import com.example.springsecurityjpademo.model.User;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.WARN)
